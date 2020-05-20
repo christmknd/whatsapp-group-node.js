29face3249e7494e66ac9e12b34e484ab16a2a34
christmknd <christmokonda@yahoo.fr>
2020-05-20 17:07:44 +0200

    ajout du routing,de la view et du css

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ app.use(express.static('public'))
 app.get('/',(req,res) => {
     res.send('Hello')
 })
+
+server = app.listen(3000)
+
+const io = require('socket.io')(server)
+
+io.on('connect', (socket) => {
+    console.log('un membre du groupe est connecté')
+})
